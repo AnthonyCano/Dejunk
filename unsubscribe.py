a36ffa72f9e497fcc8ef25a840af4feed07c42af
Anthony Cano <132002947+AnthonyCano@users.noreply.github.com>
2025-03-18 00:33:16 -0700

    Adding logic for getting user input for query_string.

--- unsubscribe.py
+++ unsubscribe.py
@@ -2,6 +2,7 @@ import re
 import webbrowser
 import time
 import os
+from delete import search_emails
 from google_apis import create_service
 
 CLIENT_FILE = "/Users/anthonycano-luna/Downloads/anthony-dejunkacct1.json"
@@ -17,22 +18,29 @@ MAX_TABS_OPEN = 3
 webbrowser.register('chrome', None, webbrowser.BackgroundBrowser("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"))
 
 
-def search_unsubscribe_emails(query_string, labels=None):
+def get_user_input():
     '''
-    Based off of the search_emails function in delete.py. Modified to find
-    emails with an unsubcribe link in the body.
+    Function to get the information of the email sender that the user would like to unsubscribe from.
+    Returns: The querey string that will then be used to search for.
     '''
-    result = gmail_service.users().messages().list(
-        userId='me',
-        q=query_string
-    ).execute()
 
-    return result.get('messages', [])
+    valid_categories = ["primary", "promotions", "social", "updates", "forums"]
+    
+    while True:
+        # Querey string for users to specify what they want to unsub from 
+        input_category = input("Please enter the email category you want to unsubcribe from. For example -> promotions:").strip().lower()
+        if input_category not in valid_categories:
+            print(f"You selcted {input_category} which is not a valid category")
+            print("Please select from one of the five valid categories: primary, promotions, social, updates, forums")
+            continue
+        input_sender =  input("Please enter the comapny name or sender of which you want to unsubscribe form. For example -> From: Chipotle:")
 
-#Hard coded query for testing
+# Hard coded query for testing
 query_string = '"unsubscribe" AND category:promotions AND newer_than:90d AND from:ticketmaster.com'
 
-email_results = search_unsubscribe_emails(query_string)
+# Call the search function in the delete script to fetch the email ids
+# that match the query_string
+email_results = search_emails(query_string)
 print(email_results)
 
 
